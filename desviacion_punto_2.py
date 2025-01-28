a3d03220f47ab2452af7b31d475f189840d0c062
juan <juanpajaro@javeriana.edu.co>
2025-01-28 12:29:30 -0500

    se agrego el punto dos al repo taller 2

---
+++ desviacion_punto_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import pandas as pd
+
+#Completar la función de desviación estandard
+def calcular_std(serie):
+    #INICIO DE CODIGO
+    #Reemplazar "None" por el cálculo de la desviación estándar
+    dev_est = None
+    #FIN DE CODIGO
+    return dev_est
